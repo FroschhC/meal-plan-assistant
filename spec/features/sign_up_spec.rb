74c2dde66d8f9ec2c290f523dce52c1c85f3ae95
chris <cefroschle@gmail.com>
2017-12-18 10:57:04 -0500

    testing indentation 2

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -9,14 +9,14 @@ feature 'sign up', %{
 
   scenario 'specify valid and required information' do
     visit new_user_registration_path
-     fill_in 'First Name', with: 'Jon'
-     fill_in 'Last Name', with: 'Smith'
-     fill_in 'Username', with: 'JSmith'
-     fill_in 'Email', with: 'user@example.com'
-     fill_in 'Password', with: 'password'
-     fill_in 'Password Confirmation', with: 'password'
-     click_button 'Sign Up'
-     expect(page).to have_content('Welcome! You have signed up successfully.')
+    fill_in 'First Name', with: 'Jon'
+    fill_in 'Last Name', with: 'Smith'
+    fill_in 'Username', with: 'JSmith'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password Confirmation', with: 'password'
+    click_button 'Sign Up'
+    expect(page).to have_content('Welcome! You have signed up successfully.')
   end
 
   scenario 'required information is not supplied' do
@@ -29,13 +29,13 @@ feature 'sign up', %{
 
   scenario 'password confirmation does not match confirmtion' do
     visit new_user_registration_path
-     fill_in 'First Name', with: 'Jon'
-     fill_in 'Last Name', with: 'Smith'
-     fill_in 'Username', with: 'JSmith'
-     fill_in 'Email', with: 'user@example.com'
-     fill_in 'Password', with: 'password'
-     fill_in 'Password Confirmation', with: 'passwor'
-     click_button 'Sign Up'
-     expect(page).to have_content("Password confirmation doesn't match")
+    fill_in 'First Name', with: 'Jon'
+    fill_in 'Last Name', with: 'Smith'
+    fill_in 'Username', with: 'JSmith'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password Confirmation', with: 'passwor'
+    click_button 'Sign Up'
+    expect(page).to have_content("Password confirmation doesn't match")
   end
 end
