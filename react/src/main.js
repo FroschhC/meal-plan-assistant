155ce2f29f13611d86a6ed388d0c7069bbcf1bed
chris <cefroschle@gmail.com>
2018-01-02 10:53:59 -0500

    app.js problems

--- react/src/main.js
+++ react/src/main.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
+import App from './app'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App/>, document.getElementById('app'));
